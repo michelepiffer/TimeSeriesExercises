975ff003d9c15b440b4e15f38f09baf97e73c7e1
michelepiffer <michelepiffer@hotmail.com>
2025-11-03 16:59:24 +0000

    added local trend model

--- LinearTrend.m
+++ LinearTrend.m
@@ -5,8 +5,8 @@ close all
 
 %% Main parameters
 
-% mu = 1;
-mu = 0;
+mu = 1;
+% mu = 0;
 
 sigma = 1;
 
@@ -27,16 +27,17 @@ u_all(30,:) = 5;
 u_all(31,:) = 5;
 
 
-%%% Variables via the random walk equation
+%%% Variables via the linear trend model
 z0 = 5;
+
 z_all      = NaN(T,N);
-z_all(1,:) = z0;
+
 
 noshocks_all = NaN(T,N);
 
-%%% Variables via the alternative equation
+%%% A model with an AR component instead of a linear trend model
 f_all      = NaN(T,N);
-
+p = 4;
 
 %%% First difference 
 firstdif_all = NaN(T,N);
@@ -47,21 +48,24 @@ for n = 1:N
 
     for t = 1:T    
     
-        %%% Via the random walk equation
+        %%% Via the linear trend model
+        z_all(t,n) = z0 + t*mu + u_all(t,n);
+
+        noshocks_all(t,n) = z0 + t*mu;
+
         if t == 1
-            z_all(t,n)        =  mu + z0 + u_all(t,n);
             firstdif_all(t,n) = z_all(t,n) - z0;
 
         else
-            z_all(t,n)        =  mu + z_all(t-1,n) + u_all(t,n);
             firstdif_all(t,n) = z_all(t,n) - z_all(t-1,n);
 
         end
 
         %%% Via the alternative equation
-        f_all(t,n) = z0 + t*mu + sum(u_all(1:t,n));
+        if t > p
+            f_all(t,n) = z0 + t*mu + sum(u_all(t-p:t,n));
+        end
 
-        noshocks_all(t,n) = z0 + t*mu;
 
     end
 end
@@ -69,6 +73,7 @@ end
 u_all        = [NaN(1,N);  u_all];
 z_all        = [z0*ones(1,N);  z_all];
 firstdif_all = [NaN(1,N);  firstdif_all];
+f_all        = [NaN(1,N);  f_all];
 
 
 %% Figures
@@ -91,11 +96,13 @@ hold on; grid on;
     % for n = 1:N
         index = plot(T_axis, u_all(:,n), 'Linewidth', 1);
     % end
-
+    % legend_index = [legend_index, index];
+    % legend_names = [legend_names; cellstr('$X_t$: level')];
 
     %%% Additional items for the figure    
     xlabel('time', 'interpreter','latex')
     set(gca, 'XTick', [0:5:T])
+    % legend(legend_index, legend_names, 'Location', 'Best', 'Interpreter', 'latex', 'AutoUpdate', 'off'); % legend('boxoff') 
     title('Shocks $u_t$', 'interpreter','latex')
     xlim([0 T])
 
@@ -109,26 +116,20 @@ hold on; grid on;
     legend_names = [];
 
     for t = 1:T
-        temp = sqrt(t*sigma^2); % standard deviation
+        temp = sqrt(sigma^2); % standard deviation
         index = line([t t], [noshocks_all(t,n)-2*temp  noshocks_all(t,n)+2*temp], 'Color', orange, 'LineWidth', 2);
     end
     legend_index = [legend_index, index(end)];
     legend_names = [legend_names; cellstr('2 standard deviations of $z_t | z_0$')];
 
     % for n = 1:N
-        index = plot(T_axis, z_all(:,n), 'o b', 'Linewidth', 1);
-        % index = plot(T_axis, z_all(:,n), 'Linewidth', 1);
+        index = plot(T_axis, z_all(:,n), '. r', 'Linewidth', 2);
     % end
     legend_index = [legend_index, index];
     % legend_names = [legend_names; cellstr('$z_t = \mu + z_{t-1} + u_t$')];
-    legend_names = [legend_names; cellstr('$z_t =  z_{t-1} + u_t$')];
+    legend_names = [legend_names; cellstr('$z_t = z_{t-1} + u_t$')];
 
-
-    index = plot(T_axis(2:end), f_all(:,n), '. r', 'Linewidth', 2);
-    legend_index = [legend_index, index];
-    % legend_names = [legend_names; cellstr('$f_t = z_0 + t \cdot \mu + sum(u_{1:t})$')];
-    legend_names = [legend_names; cellstr('$f_t = z_0 + sum(u_{1:t})$')];
-
+    % index = plot(T_axis, f_all(:,n), '-- b', 'Linewidth', 1);
 
     index = plot(T_axis(2:end), noshocks_all(:,n), '-- k', 'Linewidth', 1);
     legend_index = [legend_index, index];
@@ -182,9 +183,6 @@ set(gcf,'Position',[0 0 900*.7 900*.7]) % 3 x 1 plot
 movegui('north')
 set(gcf, 'PaperPositionMode', 'auto');
 
-% print(strcat(figures_outputpath, 'RandomWalk_withdrift'), '-dpdf')  
-% print(strcat(figures_outputpath, 'RandomWalk_withoutdrift'), '-dpdf')  
-% print(strcat(figures_outputpath, 'RandomWalk_withdrift_many'), '-dpdf')  
-% print(strcat(figures_outputpath, 'RandomWalk_withoutdrift_many'), '-dpdf')  
-
+% print(strcat(figures_outputpath, 'LinearTrend_withdrift'), '-dpdf')  
+% print(strcat(figures_outputpath, 'LinearTrend_withoutdrift'), '-dpdf')  
 
